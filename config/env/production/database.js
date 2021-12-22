bae684212fb5ddcaad012fe033c75bda59fab5a5
Hieu Tran <hieutran@Vincents-Mac.local>
2021-12-22 17:13:34 +1300

    Added Production env folder

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -19,3 +19,5 @@ module.exports = ({
     debug: false,
   },
 });
+
+// postgres://lhiodwwywogkfp:bc52c54e3b2e623c10ba2d4c037b64b9f091d376e7ba1a85b60d824363156ec0@ec2-34-204-127-36.compute-1.amazonaws.com:5432/d8k63tvs3bdmpk
