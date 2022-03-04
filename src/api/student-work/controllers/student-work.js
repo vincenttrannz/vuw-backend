d9165a81a23e14d09b891fd9f4f0ccc91a9170fd
Hieu Tran <tranhieunz@gmail.com>
2022-03-04 15:56:41 +1300

    Added new Award data

--- src/api/student-work/controllers/student-work.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  student-work controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::student-work.student-work');
--- src/api/student-work/routes/student-work.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * student-work router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::student-work.student-work');
